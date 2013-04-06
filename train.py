978615b5b96887966ae7f9330a2999c5017b1e97
Kenneth Lin <kenlin@berkeley.edu>
2013-04-06 15:27:06 -0700

    more stuff

--- train.py
+++ train.py
@@ -3,8 +3,10 @@ import nltk
 SENTIMENT_FILE = '/home/link/code/tweet-stock/data/subjclueslen1-HLTEMNLP05.tff'
 WEIGHTS = {
     "positive" : 1,
-    "neutral" : 0,
-    "negative" : -1,
+    "neutral" : 0.0,
+    "negative" : -1.0,
+    "weakneg" : -0.5,
+    "both" : 0.0,  # no way to distinguish with unigrams
     "weaksubj" : 1.0,
     "strongsubj" : 5.0
 }
@@ -14,6 +16,7 @@ SENTIMENTS = {}  # make default 0?
 with open(SENTIMENT_FILE) as f:
     for line in f:
         x = dict(x.split("=") for x in line.split())
+        print x
         SENTIMENTS[x['word1']] = WEIGHTS[x['type']] * WEIGHTS[x['priorpolarity']]
 
 print SENTIMENTS
