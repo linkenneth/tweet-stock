6ec749b6d72c088bc970caad075275a24de73eca
Jacqueline Xu <jxu703@gmail.com>
2013-04-06 17:58:52 -0700

    tweet dictionaries

--- save_tweets.py
+++ save_tweets.py
@@ -15,4 +15,20 @@ if __name__ == "__main__":
     tweets = client['tweets']
     with open("companylist.csv") as f:
         f.readline()  # skip header
-        companies = dict(csv.reader(f))
+        c = csv.reader(f)
+        nametoticker = dict()
+        wordtoname = dict()
+        for mappings in c:
+            nametoticker[string.lower(mappings[1])] = mappings[0]
+        for mappings in c:
+            namewords = string.split(string.lower(mapping[1]))
+            for word in namewords:
+                if "inc" not in word: 
+                    if word not in wordtoname:
+                        wordtoname[word] = [string.lower(mappings[1])]
+                    else:
+                        wordtoname[word] += [string.lower(mappings[1])]
+                        
+
+
+
