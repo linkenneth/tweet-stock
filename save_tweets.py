0f012088aa6351bf5652c9e71699b7a350a587f9
Kenneth Lin <kenlin@berkeley.edu>
2013-04-06 17:07:24 -0700

    updated hsit

--- save_tweets.py
+++ save_tweets.py
@@ -1,11 +1,18 @@
+import re
+import csv
 from pymongo import MongoClient
 
-client = MongoClient()
-tweets = client['tweets']
-
-def save(tweet, db):
+def save(tweet, db, words):
     """
     Saves a TWEET in the database DB if TWEET mentions contains a word from
     WORDS (mostly the names of important companies, but can be expanded
     later to do other stuff).
     """
+    tweet = 
+
+if __name__ == "__main__":
+    client = MongoClient()
+    tweets = client['tweets']
+    with open("companylist.csv") as f:
+        f.readline()  # skip header
+        companies = dict(csv.reader(f))
