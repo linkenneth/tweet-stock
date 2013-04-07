5b1232656e68146e84a0e4d155931d965c7d307c
Jacqueline Xu <jxu703@gmail.com>
2013-04-06 18:00:08 -0700

    tweet dictionaries

--- save_tweets.py
+++ save_tweets.py
@@ -13,22 +13,22 @@ def save(tweet, db, words):
 if __name__ == "__main__":
     client = MongoClient()
     tweets = client['tweets']
-    with open("companylist.csv") as f:
+    with open("data/companylist.csv") as f:
         f.readline()  # skip header
         c = csv.reader(f)
         nametoticker = dict()
         wordtoname = dict()
+        stopwords = ["inc","inc.","ltd","ltd.","co","co."]
         for mappings in c:
-            nametoticker[string.lower(mappings[1])] = mappings[0]
-        for mappings in c:
-            namewords = string.split(string.lower(mapping[1]))
+            nametoticker[mappings[1].lower()] = mappings[0]
+            namewords = (mappings[1].lower()).split()
             for word in namewords:
-                if "inc" not in word: 
+                if word not in stopwords : 
                     if word not in wordtoname:
-                        wordtoname[word] = [string.lower(mappings[1])]
+                        wordtoname[word] = [mappings[1].lower()]
                     else:
-                        wordtoname[word] += [string.lower(mappings[1])]
-                        
+                        wordtoname[word] += [mappings[1].lower()]
+        
 
 
 
